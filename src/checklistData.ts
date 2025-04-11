0e3b3907ab01933099fdd0e2622086d9b85631e7
rustywhite404 <rustywhite404@gmail.com>
2025-04-11 18:28:38 +0900

    [fix] 데이터 수정/추가 (말콤, 제롬, 제이미, 카린)

--- src/checklistData.ts
+++ src/checklistData.ts
@@ -10,7 +10,7 @@ const checklistData = [
       { npc: '라사', gives: '고급 연금술 재연소 촉매 15개', receives: '레어 연금술 재연소 촉매 1개', limit: '일 1회' },
       { npc: '레이널드', gives: '마요네즈 고기볶음 2개', receives: '치명타 비약 1개', limit: '일 1회' },
       { npc: '말콤', gives: '양털 50개', receives: '빈 악보 1개', limit: '일 1회' },
-      { npc: '말콤', gives: '말콤의 폭신한 양털 베개 1개', receives: '거미줄 5개', limit: '일 1회' },
+      { npc: '말콤', gives: '거미줄 5개', receives: '말콤의 폭신한 양털 베개 1개', limit: '일 1회' },
       { npc: '딜리스', gives: '쑥쑥버섯 1개', receives: '네잎클로버 1개', limit: '일 1회' },
       { npc: '퍼거스', gives: '분해된 장비 부품 1개', receives: '철 광석 10개', limit: '일 10회' },
       { npc: '퍼거스', gives: '분해된 장비 부품 1개', receives: '석탄 3개', limit: '일 10회' },
@@ -44,10 +44,11 @@ const checklistData = [
       { npc: '발터', gives: '어둠유령고기 5개', receives: '바람 마법 유탄 3개', limit: '일 3회' },
       { npc: '발터', gives: '어둠유령고기 5개', receives: '산성 마법 유탄 3개', limit: '일 3회' },
       { npc: '시몬', gives: '상급 실크 10개', receives: '염색약 베이스 1개', limit: '일 1회' },
-      { npc: '제롬', gives: '리코타 치즈 샐러드 1개', receives: '실크 4개', limit: '일 1회' },
-      { npc: '제롬', gives: '크림소스 스테이크 1개', receives: '상급 실크 2개', limit: '일 1회' },
+      { npc: '제롬', gives: '리코타 치즈 샐러드 1개', receives: '실크 4개', limit: '일 2회' },
+      { npc: '제롬', gives: '크림소스 스테이크 1개', receives: '상급 실크 4개', limit: '일 1회' },
       { npc: '제이미', gives: '사과 수플레 1개', receives: '상급 옷감 4개', limit: '일 2회' },
-      { npc: '칼릭스', gives: '글리니스의 애플 밀크티 3개', receives: '상급 목재 12개', limit: '일 2회' }
+      { npc: '제이미', gives: '사과 생크림 케이크 1개', receives: '상급 옷감+ 4개', limit: '일 1회' },
+      { npc: '칼릭스', gives: '글리니스의 애플 밀크티 3개', receives: '상급 목재+ 12개', limit: '일 2회' }
     ]
   },
   {
@@ -74,6 +75,12 @@ const checklistData = [
       { npc: '케아라', gives: '상급 생가죽+ 10개', receives: '협곡의 마나석1개', limit: '일 3회' },
       { npc: '케아라', gives: '상급 생가죽+ 10개', receives: '페허의 마나석 1개', limit: '일 3회' },
       { npc: '케아라', gives: '얼음 딸기주스 2개', receives: '상급 치명타 비약 1개', limit: '일 1회' }
+    ]
+  },
+  {
+    region: '얼음 협곡 임시 초소',
+    items: [
+      { npc: '카린', gives: '감자수프 3개', receives: '펫먹이 10개', limit: '일 1회' }
     ]
   }
 ];
